e61905f7cb71e89cc3d46222200a6674242c4f87
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2021-08-28 14:47:55 -0300

    chamando os tipos e imagens da api

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -8,12 +8,17 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
 
+  public getAllPokemons: any;
+
   constructor(
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons. subscribe(res => console.log(res));
+    this.pokeApiService.apiListAllPokemons. subscribe(
+      res => {
+      this.getAllPokemons = res.results
+      });
   }
 
 }

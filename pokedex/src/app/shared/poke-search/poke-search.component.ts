7e2d663b8cdab6280a8c5b684c80445b179718b9
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2021-08-30 23:45:35 -0300

    fazendo campo search armazenar valor string

--- pokedex/src/app/shared/poke-search/poke-search.component.ts
+++ pokedex/src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'poke-search',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-search.component.scss']
 })
 export class PokeSearchComponent implements OnInit {
-
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public search(value: string){
+    this.emmitSearch.emit(value);
+  }
 }

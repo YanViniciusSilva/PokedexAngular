bc8ca375c55c297c0859d18b97e01a1f3d801d0e
YanViniciusSilva <yanviniciusjesussilva@gmail.com>
2021-09-04 18:05:21 -0300

    alteração das imagem dos pokemons

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -20,7 +20,7 @@ export class PokeListComponent implements OnInit {
       res => {
       this.setAllPokemons = res.results;
       this.getAllPokemons = this.setAllPokemons;
-      //console.log(this.setAllPokemons);
+      console.log(this.setAllPokemons);
       }
     );
   }
